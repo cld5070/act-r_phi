fb36554a0205fa8301c6956248758bc504a32f07
CD <4346176+cld5070@users.noreply.github.com>
2020-01-05 21:11:39 -0500

    Working on updating so that sleep deprivations eqns work w/ newer phys model. Cortisol,DES and ACTH.DES appear to be OK now; CRF a WIP - Need to make it work with the new metric for CRF effects. Next step, testing the changes in using HumMod

--- environment/GUI/init/05-current-net.tcl
+++ environment/GUI/init/05-current-net.tcl
@@ -1,3 +1,3 @@
-# Port settings for ACT-R server started at 13:20:40 11/26/2019
-set actr_port 2651
+# Port settings for ACT-R server started at 17:17:41 1/05/2020
+set actr_port 2650
 set actr_address "10.0.75.1"
