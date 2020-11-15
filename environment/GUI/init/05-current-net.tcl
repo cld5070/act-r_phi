db67a697d68ef5139bb5b7e30dab3ad595226fe8
CD <4346176+cld5070@users.noreply.github.com>
2020-11-15 17:29:25 -0500

    Small changes

--- environment/GUI/init/05-current-net.tcl
+++ environment/GUI/init/05-current-net.tcl
@@ -1,3 +1,3 @@
-# Port settings for ACT-R server started at 12:06:06 11/15/2020
-set actr_port 2650
+# Port settings for ACT-R server started at 12:18:28 11/15/2020
+set actr_port 2658
 set actr_address "10.0.243.15"
