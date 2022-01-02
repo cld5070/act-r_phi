783a2d7721c57d7f743ef1dfc4535a43bfe2db11
CD <4346176+cld5070@users.noreply.github.com>
2022-01-02 14:39:35 -0500

    No notable changes

--- environment/GUI/init/05-current-net.tcl
+++ environment/GUI/init/05-current-net.tcl
@@ -1,3 +1,3 @@
-# Port settings for ACT-R server started at 14:35:48 12/28/2020
-set actr_port 2650
+# Port settings for ACT-R server started at 07:21:41 5/31/2021
+set actr_port 2664
 set actr_address "192.168.1.118"
--- environment/GUI/init/10-userguisettings.tcl
+++ environment/GUI/init/10-userguisettings.tcl
@@ -15,8 +15,22 @@ if $size_mismatch {
                                          -message "The screen resolution is not the same as it was the last time the Environment was used.  Should the window positions reset to the defaults?"]
 } else { set reset_window_sizes 0}
 if {$reset_window_sizes != "yes"} {
+  set window_config(.pgraph) 1581x400+1370+520
+  set changed_window_list(.pgraph) 1
+  set window_config(.dispatcher) 700x274+1370+583
+  set changed_window_list(.dispatcher) 1
+  set window_config(.pick_buffers) 200x340+1620+550
+  set changed_window_list(.pick_buffers) 1
   set window_config(.control_panel) 235x700+3185+370
   set changed_window_list(.control_panel) 1
+  set window_config(.options) 450x274+1495+583
+  set changed_window_list(.options) 1
+  set window_config(.text_trace_history) 714x340+1333+550
+  set changed_window_list(.text_trace_history) 1
+  set window_config(.procedural) 500x400+1470+520
+  set changed_window_list(.procedural) 1
+  set window_config(.bold_graphs) 660x250+1390+600
+  set changed_window_list(.bold_graphs) 1
 }
 set gui_options(p_selected) #44DA22
 set gui_options(p_matched) #FCA31D
